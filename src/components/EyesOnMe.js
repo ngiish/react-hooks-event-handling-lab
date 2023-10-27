9a6d34aa2f035453945ac7a1738ba291209bb6de
Isaac <govegeku00@gmail.com>
2023-10-27 20:43:09 +0300

    Completed Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+    function eyesOnTheBallFocus(event){
+        console.log('Good!')
+    }
+
+    function eyesOnTheBallBlur(event){
+        console.log('Hey! Eyes on me!')
+    }
+  return (
+    <div className='buttons'>
+        <button onFocus={eyesOnTheBallFocus} onBlur={eyesOnTheBallBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
